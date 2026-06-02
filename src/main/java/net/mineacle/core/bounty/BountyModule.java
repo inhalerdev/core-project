d41172786f3e321f9ea85368cf1ce84c7ab0ed2d
Inhaler <brandinsarazin@gmail.com>
2026-06-02 15:38:12 -0500

    Update 6/2

--- src/main/java/net/mineacle/core/bounty/BountyModule.java
+++ src/main/java/net/mineacle/core/bounty/BountyModule.java
@@ -2,15 +2,16 @@ package net.mineacle.core.bounty;
 
 import net.mineacle.core.Core;
 import net.mineacle.core.bootstrap.Module;
+import net.mineacle.core.bounty.command.BountyCommand;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
 
 public final class BountyModule extends Module {
 
-    private static BountyService bountyService;
+    private static net.mineacle.core.bounty.BountyService bountyService;
 
-    public static BountyService bountyService() {
+    public static net.mineacle.core.bounty.BountyService bountyService() {
         return bountyService;
     }
 
@@ -21,13 +22,13 @@ public final class BountyModule extends Module {
 
     @Override
     public void enable(Core core) {
-        bountyService = new BountyService(core);
+        bountyService = new net.mineacle.core.bounty.BountyService(core);
 
         BountyCommand command = new BountyCommand(core, bountyService);
         register(core, "bounty", command);
 
-        core.getServer().getPluginManager().registerEvents(new BountyListener(core, bountyService), core);
-        core.getServer().getPluginManager().registerEvents(new BountyGuiListener(core, bountyService), core);
+        core.getServer().getPluginManager().registerEvents(new net.mineacle.core.bounty.BountyListener(core, bountyService), core);
+        core.getServer().getPluginManager().registerEvents(new net.mineacle.core.bounty.BountyGuiListener(core, bountyService), core);
     }
 
     @Override
